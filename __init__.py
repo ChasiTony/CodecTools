b2f689f0eaf448ce79a911ddbde77570336d7fe1
Guangwei Wang <guangwwa@cisco.com>
2015-08-04 13:36:02 +0800

    modify for downsampler test

--- __init__.py
+++ __init__.py
@@ -3,6 +3,10 @@
 TMP_FOLDER = "/Users/sijchen/WorkingCodes/TestSequences/tmp"
 CAMERA_SEQ_PATH = "/Users/sijchen/WorkingCodes/TestSequences/CameraTypical"
 SCREEN_SEQ_PATH = "/Users/sijchen/WorkingCodes/TestSequences/ScreenTypical"
+#Add by guangwei
+TEST_SEQ_PATH = '/Users/guangwwa/WorkSpace/TestSequences/'
+OUT_DATA_PATH = '/Users/guangwwa/WorkSpace/IOdata/'
+TOOLS_PATH = '/Users/guangwwa/WorkSpace/Tools/'
 
 def get_did_from_resolution(width, height):
     reso = width+height
